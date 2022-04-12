d4699b16be4313d105fb09831dcf6d1687b90f70
Calvahar <58914729+Calvahar@users.noreply.github.com>
2022-04-12 14:09:32 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/websocket/v2"
 
-	"net/http"
-
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 )
 
 func main() {
@@ -23,71 +22,79 @@ func main() {
 
 	app.Use("/ws", func(c *fiber.Ctx) error {
 		// Source: Fiber Documentation
-		// IsWebSocketUpgrade returns true if the client
-		// requested upgrade to the WebSocket protocol.
+		// Kijk of de HTTP request de `Upgrade: websocket` headers heeft.
+		// Voeg de API_KEY en USERNAME headers toe aan Locals.
 		if websocket.IsWebSocketUpgrade(c) {
 			c.Locals("allowed", true)
+			c.Locals("header_KEY", c.Get("API_KEY"))
+			c.Locals("header_USERNAME", c.Get("USERNAME"))
+
+			// Ga naar de volgende /ws endpoint
 			return c.Next()
 		}
+
+		// Als de connectie geen Upgrade header met WebSocket heeft, stuur een error terug.
 		return fiber.ErrUpgradeRequired
 	})
 
 	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
-		// IoT device stuurt verzoek naar /ws
-		// WebSocket verbinding wordt geopened.
-
-		// Wij gebruiken de Headers van dat verzoek en roepen authReq aan
-
-		// Antwoord van de response
-
-		// Mag WEL een verbinding houden, laat de verbinding open staan
-
-		// Mag NIET een verbinding houden, drop de verbinding
+		// Sla de API_KEY en Username op in een variable
+		api_key := ""
+		username := ""
+		if c.Query("key") != "" && c.Query("username") != "" {
+			api_key = c.Query("key")
+			username = c.Query("username")
+		} else if c.Locals("header_KEY") != nil && c.Locals("header_USERNAME") != nil {
+			api_key = fmt.Sprintf("%s", c.Locals("header_KEY"))
+			username = fmt.Sprintf("%s", c.Locals("header_USERNAME"))
+		}
 
 		client := &http.Client{}
-		req, _ := http.NewRequest("GET", "http://localhost:3000/test", nil)
-		req.Header.Set("API_KEY", "idiiwadiwaj")
-		req.Header.Set("USERNAME", "test")
-		res, _ := client.Do(req)
 
-		// fmt.Println(res.Request.Header)
-		fmt.Println(res.Request.Header)
+		// url: De auth server.
+		url := "http://localhost:3000/test"
+		req, _ := http.NewRequest("GET", url, nil)
 
-		for {
-			mt, msg, err := c.ReadMessage()
-			if err != nil {
-				log.Println("read:", err)
-				break
-			}
-			log.Printf("recv: %s", msg)
-			err = c.WriteMessage(mt, msg)
-			if err != nil {
-				log.Println("write:", err)
-				break
+		// Stuurt de API key en Username mee naar de auth server.
+		req.Header.Set("API_KEY", api_key)
+		req.Header.Set("USERNAME", username)
+
+		res, err := client.Do(req)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// Hou de connectie open, wacht op antwoord van de auth server.
+		defer res.Body.Close()
+
+		body, _ := ioutil.ReadAll(res.Body)
+
+		// @TODO: verander string in Allow || Deny
+		if string(body) == "Ja dat klopt" {
+			// c.Conn.Close()
+			for {
+
+				// err = c.WriteMessage(mt, msg)
+				// if err != nil {
+				// 	log.Println("write:", err)
+				// 	break
+				// }
 			}
 		}
 	}))
 
-	app.Get("/panic", func(c *fiber.Ctx) error {
-		panic("paniek")
-	})
-
+	//Test authenticatie
 	app.Get("/test", func(c *fiber.Ctx) error {
-		// fmt.Println(c.Request().Header)
-		fmt.Println(c.GetRespHeaders())
-		c.Set("KEY", "STRING")
-		return c.SendString("a")
+		api_key := c.Get("API_KEY") == "test"
+		username := c.Get("USERNAME") == "test"
+
+		if username != true || api_key != true {
+			return c.SendString("Klopt niet")
+		}
+		return c.SendString("Ja dat klopt")
 	})
 
+	// Luister naar localhost op poort `3000`
 	log.Fatal(app.Listen(":3000"))
 }
-
-// func authReq(app fiber.App, API_KEY string, USERNAME string) bool {
-// 	// localhost
-// 	// token := gestuurde api key
-//
-// 	// Auth: API_KEY
-// 	// USERNAME: MijnIOTDevice
-
-// 	return true
-// }
